e10fea028b412efdc3b57ca45717e3e343525ba1
CESAR TZOC <csartalva01@gmail.com>
2020-01-16 00:51:39 -0600

    Se inicio la creacion de la ventana principal

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -5,17 +5,29 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import object.Socios;
 
 /**
  * FXML Controller class
@@ -44,8 +56,25 @@ public class LoginController implements Initializable {
 
     @FXML
     void iniciarSesion(ActionEvent event) {
-        //Evento Iniciar Sesion
         
+        //Evento Iniciar Sesion
+        String user = text_socio.getText();
+        String pass = pass_socio.getText();
+            
+//        EntityManagerFactory emf = conexion.ConexionJPA.getInstancia().getEMF();
+//        EntityManager em = emf.createEntityManager();
+//        
+//        Query consultaLogin = em.createNamedQuery("Socios.findAll");
+//        List<Socios> socios = (List<Socios>) consultaLogin.getResultList();
+//        for(Socios s: socios){
+//            s.getCodigo();
+//        }
+
+        try {
+            getMainView();
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     /**
@@ -57,6 +86,15 @@ public class LoginController implements Initializable {
         URL nuevo = getClass().getResource("/img/icono200x200.png");
         Image image = new Image(nuevo.toString(), 200, 200, false, true);
         image_view.setImage(image);
-    }    
+    }
 
+    public void getMainView() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("../view/MainView.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Inicio");
+        stage.setScene(new Scene(root));
+        stage.show();
+        
+        label_socio.getScene().getWindow().hide();
+    }
 }
