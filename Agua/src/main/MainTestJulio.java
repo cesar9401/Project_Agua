324db57c0aec09fa64b096dbeaef0010ddded04f
Julio <juliofernando.ixcoy@gmail.com>
2020-01-19 18:12:43 -0600

    se agrego la vista de Modificar y ver todos los socios, en modificar carga los datos de los socios a modificar

--- Agua/src/main/MainTestJulio.java
+++ Agua/src/main/MainTestJulio.java
@@ -24,7 +24,7 @@ public class MainTestJulio extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../view/CrearSocio.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/ModifySocio.fxml"));
         stage.setTitle("Login");
         stage.setScene(new Scene(root));
         stage.show();
